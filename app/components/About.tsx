77fa3af41273d828ed79750b30f6bfcd7ebb3934
Danawl <awildandan@gmail.com>
2025-10-22 14:35:40 +0800

    fix about

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,11 +6,11 @@ export default function About() {
         <div className="max-w-5xl mx-auto px-6">
           <h2 className="text-4xl font-semibold mb-6">About</h2>
           <p className="text-primary mb-6">
-            This is a brand new, fully furnished studio-type condo unit available for rent. Ready for occupancy, it offers a comfortable and convenient living space—perfect for individuals or couples looking for a hassle-free move-in experience.
+            This is a brand new, fully furnished studio-type condo unit available for rent. Ready for occupancy, it offers a comfortable and convenient living space that is perfect for individuals or couples looking for a hassle-free move-in experience.
           </p>
           <p className="text-primary">
-            With thoughtfully designed units, contemporary architecture, world-class range of amenities—including a
-            swimming pool, fitness center, function rooms, and 24/7 security—our condominium is built to support a balanced lifestyle. Whether you’re looking for a peaceful home close to the city or a valuable property investment, this community near Alabang is the perfect place to start.
+            With thoughtfully designed units, contemporary architecture, world-class range of amenities including a
+            swimming pool, fitness center, function rooms, and 24/7 security—our condominium is built to support a balanced lifestyle. Whether {"you're"} looking for a peaceful home close to the city or a valuable property investment, this community near Alabang is the perfect place to start.
           </p>
         </div>
       </section>
